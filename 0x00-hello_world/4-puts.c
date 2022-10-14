d585b485906bc3f58b4b30035827a613660cc719
WALID <walidmurad65@gmail.com>
2022-10-14 06:38:38 -0700

    MULTILINGUAL

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - prints the phrase "programming is like building a multilingual puzzle"
- *
- *   *
- *
- *    * Return: Always 0 if exited properly, non-zero otherwise
- *
- *     */
-
+ * main - Entry point
+ *  
+ * Return: Always 0 
+ */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
