83ab66672f5bc537a8225a4720317cf54b33c27d
WALID <walidmurad65@gmail.com>
2022-10-18 06:47:38 -0700

    help

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,35 +1,15 @@
-#include "holberton.h"
-
-
-
+#include "main.c"
 /**
+ *print_alphabet - Make the alphabet
  *
- *   * print_alphabet - Make the alphabet
- *
- *     *
- *
- *       * Return: void
- *
- *         */
-
+ * Return: void
+ */
 void print_alphabet(void)
-
 {
-
 		char c;
-
-
-
 			for (c = 'a'; c <= 'z'; c++)
-
-					{
-
+			{
 								_putchar(c);
-
 									}
-
-
-
 				_putchar('\n');
-
 }
