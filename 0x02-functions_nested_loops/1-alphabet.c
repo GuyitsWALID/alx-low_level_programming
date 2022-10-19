8940fcc8baf5ae81aae9987f2ce49a2c99e221da
WALID <walidmurad65@gmail.com>
2022-10-19 02:27:05 -0700

    gretu

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,11 +10,11 @@
  */
 void print_alphabet(void)
 {
-char letter = 'a';
-while (letter <= 'z')
+              	char letter = 'a';
+              	while (letter <= 'z')
 	{
-	      _putchar(letter);
-	      letter++;
+	    	 _putchar(letter);
+	      	letter++;
 	}
               _putchar('\n');
 }
